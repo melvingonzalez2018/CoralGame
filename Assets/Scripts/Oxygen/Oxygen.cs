0f375c9843c1c46d6e59b2557c96e796f81528d3
Nelson Lieu <36254391+TheStress@users.noreply.github.com>
2024-06-28 00:12:39 -0400

    Oxygen, puffer fish, trash, end scree, level select (#11)
    * added stuff
    
    - oxygen + UI
    - level select
    - end screen, just a restart button
    
    * added puffer fish and trash

---
+++ Assets/Scripts/Oxygen/Oxygen.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Oxygen : MonoBehaviour
+{
+    [SerializeField] public UnityEvent OnOxygenDuration = new UnityEvent();
+    [SerializeField] public float oxygenDuration;
+    [HideInInspector] public float timer = 0;
+    bool runTimer = false;
+    private void Start() {
+        StartTime(); // Starting time for testing
+    }
+
+    private void Update() {
+        if(runTimer) {
+            timer = Mathf.Min(timer+Time.deltaTime, oxygenDuration);
+            if(timer >= oxygenDuration) {
+                OnOxygenDuration.Invoke();
+                runTimer = false;
+            }
+        }
+    }
+
+    public void StartTime() {
+        runTimer = true;
+    }
+
+    public void ReduceOxygen(float amount) {
+        timer = Mathf.Min(timer + amount, oxygenDuration);
+        if (timer >= oxygenDuration) {
+            OnOxygenDuration.Invoke();
+            runTimer = false;
+        }
+    }
+
+}
