a5dab465f1f82d38d2411da295ddf607ebe6a9da
Nelson Lieu <36254391+TheStress@users.noreply.github.com>
2024-07-03 23:04:52 -0400

    Water currents player adjustments (#34)
    * fixed currents, camera adjustments, and movement update
    
    * applied to prefabs

--- Assets/Scripts/WaterCurrents/WaterCurrents.cs
+++ Assets/Scripts/WaterCurrents/WaterCurrents.cs
@@ -7,9 +7,15 @@ public class WaterCurrents : MonoBehaviour
     [SerializeField] Transform currentDirection;
     [SerializeField] float currentForce;
     [SerializeField] string playerTag;
+    PlayerMovementController movementController;
+
+    private void Start() {
+        movementController = FindObjectOfType<PlayerMovementController>();
+    }
+
     private void OnTriggerStay(Collider other) {
-        if(other.tag == playerTag) {
-            other.GetComponent<Rigidbody>().velocity += currentForce * currentDirection.forward * Time.deltaTime;
+        if (other.tag == playerTag && movementController != null) {
+            movementController.AddVelocity(currentForce * currentDirection.forward);
         }
     }
 }
