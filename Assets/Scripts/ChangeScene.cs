fff0bd7fe4b2972d30bb6e3b1791b1f698a010d3
TheStress <lieu.nelson386@gmail.com>
2024-07-13 03:24:16 -0400

    added mainmenu

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    [SerializeField] bool clickToChangeScene = false;
+    [SerializeField] string sceneName;
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0) && clickToChangeScene) {
+            StartChangeScene(sceneName);
+        }
+    }
     public void StartChangeScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
