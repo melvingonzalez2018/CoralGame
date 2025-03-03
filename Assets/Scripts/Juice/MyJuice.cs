1359078d6b29d965e30d6ca508cbe1cb0808e719
Nelson Lieu <36254391+TheStress@users.noreply.github.com>
2025-03-03 00:06:19 -0500

    UI polish (#102)
    * button wobble
    
    * added click effect

---
+++ Assets/Scripts/Juice/MyJuice.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public abstract class MyJuice : MonoBehaviour {
+    public float duration;
+    public float scale;
+    public UnityEvent OnCompleteEffect = new UnityEvent();
+    float timer = 0;
+    bool effectActive = false;
+
+    protected abstract void EffectUpdate(float val, float valScale);
+    protected abstract float EaseFunction(float t);
+    protected abstract void ResetValue();
+
+    public void AddOnComplete(UnityAction onCompleteAction) {
+        OnCompleteEffect.AddListener(onCompleteAction);
+    }
+
+    public void Activate() {
+        timer = duration;
+        effectActive = true;
+    }
+
+    public void Cancel() {
+        EndOfEffect();
+    }
+
+    private void Update() {
+        if (effectActive) {
+            timer -= Time.deltaTime;
+
+            EffectUpdate(EaseFunction(1 - (timer / duration)), scale);
+
+            if (timer <= 0) {
+                EndOfEffect();
+            }
+        }
+    }
+
+    private void EndOfEffect() {
+        OnCompleteEffect.Invoke();
+        ResetValue();
+        effectActive = false;
+        timer = duration;
+    }
+
+}
